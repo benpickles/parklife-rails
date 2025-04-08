84467684b261dfa8215aab05056a15229ba615fb
Ben Pickles <spideryoung@gmail.com>
2025-04-08 21:25:39 +0100

    Move stuff into separate files

---
+++ lib/parklife/rails/railtie.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+require 'parklife'
+require 'rails'
+require_relative 'config_refinements'
+require_relative 'route_set_refinements'
+
+module Parklife
+  module Rails
+    class Railtie < ::Rails::Railtie
+      initializer 'parklife.integration' do |app|
+        # The offending middleware is included in Rails (6+) development mode and
+        # rejects a request with a 403 response if its host isn't present in the
+        # allowlist (a security feature). This prevents Parklife from working in
+        # a Rails app out of the box unless you manually add the expected
+        # Parklife base to the hosts allowlist or set it to nil to disable it -
+        # both of which aren't great because they disable the security feature
+        # whenever the development server is booted.
+        #
+        # https://guides.rubyonrails.org/configuring.html#actiondispatch-hostauthorization
+        #
+        # However it's safe to remove the middleware at this point because it
+        # won't be executed in the normal Rails development flow, only via a
+        # Parkfile when parklife/rails is required.
+        if defined?(ActionDispatch::HostAuthorization)
+          app.middleware.delete(ActionDispatch::HostAuthorization)
+        end
+
+        Parklife.application.config.app = app
+
+        # This ensures next tweak is compatibile with Rails 8+ lazy routes.
+        Parklife.application.routes.extend(RouteSetRefinements)
+
+        # Allow use of the Rails application's route helpers when defining
+        # Parklife routes in the block form.
+        Parklife.application.routes.singleton_class.include(app.routes.url_helpers)
+
+        Parklife.application.config.extend(ConfigRefinements)
+      end
+
+      config.after_initialize do |app|
+        # Read the Rails app's URL config and apply it to Parklife's so that the
+        # Rails config can be used as the single source of truth.
+        host, port, protocol = app.default_url_options.values_at(:host, :port, :protocol)
+        protocol = 'https' if app.config.force_ssl
+        path = ActionController::Base.relative_url_root
+
+        Parklife.application.config.base.scheme = protocol if protocol
+        Parklife.application.config.base.host = host if host
+        Parklife.application.config.base.port = port if port
+        Parklife.application.config.base.path = path if path
+      end
+    end
+  end
+end
